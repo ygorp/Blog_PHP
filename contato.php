b2bc0b490a7e0a5404766c2ee3f16cbbe8061f43
ygorp <ygor-nogueira@hotmail.com>
2025-05-21 18:40:47 -0300

    estilizando a home

---
+++ contato.php
@@ -0,0 +1,9 @@
+<?php
+  include_once("templates/header.php");
+?>
+
+
+
+<?php
+  include_once("templates/footer.php");
+?>
--- index.php
+++ index.php
@@ -2,6 +2,26 @@
   include_once("templates/header.php");
 ?>
 
+<main>
+  <div id="title-container">
+    <h1>Blog webtech</h1>
+    <p>Seu blog de programação</p>
+  </div>
+  <div id="posts">
+    <?php foreach($posts as $post): ?>
+      <div class="post-box">
+        <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+        <h2 class="post-title"><a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a></h2>
+        <p class="post-description"><?= $post['description'] ?></p>
+        <div class="tags-container">
+          <?php foreach($post['tags'] as $tag): ?>
+            <a href="<?= $BASE_URL ?>tags.php?q=<?= $tag ?>" class="tag"><?= $tag ?></a>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</main>
 
 <?php
   include_once("templates/footer.php");
